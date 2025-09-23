918045e8400fc716c8c7a8a9672d96205d0af3d6
Anisha Rawat <anisharawat324@gmail.com>
2025-09-23 22:08:01 +0530

    Create ContainerWithMostWater.cpp

---
+++ ContainerWithMostWater.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i=0;
+        int j=height.size()-1;
+        long long prod=0;
+        while(i<j){
+            long long area=(j-i)*min(height[i],height[j]);
+            prod=max(prod,area);
+            if(height[i]<height[j]){
+                i++;
+            }
+            else
+                j--;
+        }
+        return prod;
+    }
+};
