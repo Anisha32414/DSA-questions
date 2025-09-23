bbe3805266cdf9ecb1bc1caa7ea4fc75d209c325
Anisha Rawat <anisharawat324@gmail.com>
2025-09-23 21:39:35 +0530

    Create 3Sum.cpp

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        
         sort(nums.begin(),nums.end());
         int n=nums.size();
         vector<vector<int>> result;
+        
         for(int i=0;i<n;i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
             int j=i+1;
             int k=n-1;
+            
             while(j<k){
                 int sum=nums[i]+nums[j]+nums[k];
                 if(sum>0)
                    k--;
                 else if(sum<0)
                    j++;
+                    
                 else{
                     result.push_back({nums[i],nums[j],nums[k]});
                     j++;
                     k--;
                     while(j<n && nums[j]==nums[j-1]) j++;
                     while(j<n && nums[k]==nums[k+1]) k--;
+                    
                 }
             }
         }
