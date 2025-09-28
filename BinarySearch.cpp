0c73ffc51a2dfc2db9c42630dc74ba7a5ef9525b
Anisha Rawat <anisharawat324@gmail.com>
2025-09-28 22:45:04 +0530

    Create BinarySearch.cpp

---
+++ BinarySearch.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int first=0;
+        int last=n-1;
+        for(int i=0;i<n;i++){
+            int mid=first+(last-first)/2;
+            if(nums[mid]==target)
+               return mid;
+            else if(nums[mid]>target){
+                last=mid-1;
+            }
+            else
+                first=mid+1;
+        }
+        return -1;
+    }
+};
