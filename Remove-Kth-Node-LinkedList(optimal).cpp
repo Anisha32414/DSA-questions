f0e10c8279541b29cbbb0ae49a72df130ba432a2
Anisha Rawat <anisharawat324@gmail.com>
2025-10-26 22:38:48 +0530

    Create Remove-Kth-Node-LinkedList(optimal).cpp

---
+++ Remove-Kth-Node-LinkedList(optimal).cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy=new ListNode(0);
+        dummy->next=head;
+        ListNode* s=dummy;
+        ListNode* f=dummy;
+        for(int i=0;i<=n;i++){
+            f=f->next;
+        }
+        while(f!=NULL){
+            s=s->next;
+            f=f->next;
+        }
+        ListNode* del=s->next;
+        s->next=s->next->next;
+        del->next=NULL;
+        delete del;
+        ListNode* newHead=dummy->next;
+        delete dummy;
+        return newHead;
+        
+    }
+};
