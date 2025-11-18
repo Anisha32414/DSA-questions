398103bdb48e86af22e69b9dcb5abdea4fdf33ba
Hello World !!! <anisharawat324@gmail.com>
2025-11-18 22:41:59 +0530

    Create (78)-Subsets.cpp

---
+++ (78)-Subsets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+
+    void printSubsets(int i,vector<int>&nums,vector<vector<int>>&ans,vector<int>&arr){
+        if(i>=nums.size()){
+            ans.push_back(arr);
+            return;
+        }
+        arr.push_back(nums[i]);
+        printSubsets(i+1,nums,ans,arr);
+        arr.pop_back();
+        printSubsets(i+1,nums,ans,arr);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>arr;
+        printSubsets(0,nums,ans,arr);
+        return ans;
+    }
+};
