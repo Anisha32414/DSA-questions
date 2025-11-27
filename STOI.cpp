af405f25fb5e1a7627ef9d5aa0f9015eee253116
Hello World !!! <anisharawat324@gmail.com>
2025-11-27 20:32:35 +0530

    Create STOI.cpp

---
+++ STOI.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int n=s.size();
+        int sign=1;
+        long res=0;
+        int i=0;
+        while(i<n && s[i]==' '){
+            i++;
+        }
+        if(s[i]=='-') {
+            sign=-1;
+            i++;
+        }
+        else if(s[i]=='+'){
+            i++;
+        }
+
+        while(i<n && isdigit(s[i])){
+            
+            res=res*10+(s[i]-'0');
+
+            if(sign*res > INT_MAX) return INT_MAX;
+            if(sign*res < INT_MIN) return INT_MIN;
+            
+            i++;
+        }
+        
+        return (int)(sign * res);
+    }
+};
